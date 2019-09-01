1a7b2ac8429ed6fb0ed309c359f3a8b89d45dd1b
subhra paladhi <subhrapaladhi9@gmail.com>
2019-09-01 16:18:33 +0530

    using .env file to hide credentials and other similar data

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const   express     = require('express'),
         mongoose    = require('mongoose'),
         app         = express();
 
+// loading the environment variables
+require('env2')('.env');
+
 // setting body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,25 +18,24 @@ app.use(express.static(publicDir));
 
 // setup for creating session and other related stuff with authentication
 app.use(session({
-    secret: 'fldsk2232',
+    secret: process.env.secret,
     resave: true,
     saveUninitialized: true
 }))
 
 let auth = function(req,res,next){
-    if(req.session && req.session.password == password)
+    if(req.session && req.session.loggedIn)
         return next();      // if a valid session is already running then continue 
     
     else
         return res.redirect('/');       // otherwise redirect to login form page
 }
 
-const password = "abc123"; 
+const password = process.env.password; 
 
 
 // setting up db
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/webinar', {useNewUrlParser: true});
+mongoose.connect(process.env.mongoURL, {useNewUrlParser: true});
 mongoose.connection
     .once('open', () => console.log('connected to database'))
     .on('error', (error) => console.log('connection to database failed. Error = ', error));
@@ -48,7 +50,7 @@ const Webinar = require('./models/webinar');
 app.get('/', (req,res) => {
     // render the login form
 
-    // res.render('login.ejs')
+    res.render('login.ejs')
 })
 
 app.post('/', (req,res) => {
@@ -57,8 +59,8 @@ app.post('/', (req,res) => {
     console.log(enteredPassword)
     if(password == enteredPassword){
         console.log('logged in');
-        req.session.password = enteredPassword;
-        res.send('/home');
+        req.session.loggedIn = true;
+        res.redirect('/home');
     }
     else{
         console.log('wrong password');
@@ -71,13 +73,13 @@ app.get('/home', auth, (req,res) => {
                     .then((allWebinars) => {
                         res.json({webinars: allWebinars})
                     })
-                    .catch((err) => console.log('cant get the data'));
+                    .catch((err) => console.log('cant get the data. error = ',err));
 })
 
 app.get('/home/newWebinar', auth, (req,res) => {
     //render the new webinar page
     
-    // res.render('newWebinar.ejs')
+    res.render('newWebinar.ejs')
 })
 
 app.post('/home/newWebinar', auth, (req,res) => {
@@ -90,15 +92,18 @@ app.post('/home/newWebinar', auth, (req,res) => {
     }
     
     new Webinar(newWebinar).save()
-                            .then((temp) =>console.log(temp))
-                            .catch((err) => console.log(err))
-
-    res.send('home/newWebinar-post')
+                            .then((temp) =>{
+                                console.log(temp)
+                                res.redirect('/home')
+                            })
+                            .catch((err) => console.log(err))   
 })
 
 app.get('/logout', auth, (req,res) => {
+    // on clicking the logout button
     req.session.destroy();
-    res.send('logout success');
+    res.redirect('/');
 })
+
 
 app.listen(3000,()=>console.log("connected to port 3000"))
