0782cb791ea45b2b528d7808fc91fc96094e44c0
subhra paladhi <subhrapaladhi9@gmail.com>
2019-09-11 21:08:02 +0530

    making backend compatible for react js in frontend

--- mail.js
+++ mail.js
@@ -1,40 +1,41 @@
 var nodemailer = require('nodemailer')
 var webinar=require('./models/emailIdsListModel')
-var send=[]
+var send=[];
 
 webinar.find({}).then((emails)=>{
   send=emails
 })
 
 const options={
-        pool:true,
-        host:'smtp.gmail.com',
-        port:587,
-        secure:false,
-        auth:{
-            user:process.env.email,
-            pass:process.env.password
-        }
-    }
+  pool:true,
+  host:'smtp.gmail.com',
+  port:587,
+  secure:false,
+  auth:{
+      user:process.env.email,
+      pass:process.env.password
+  }
+};
 
-    var transporter=nodemailer.createTransport(options,null)
-      
-      var mailOptions = {
-        from: 'senders email',
-        to: send,
-        subject: 'testing',
-        text: 'your message',
-        attachments:[
-          {
-            filename:'filename of attachment',
-            path:'path for attachment'
-          }
-        ]
-      };
-      transporter.sendMail(mailOptions, function(error, info){
-          if (error) {
-            console.log(error);
-          } else {
-            console.log('Email sent: ' + info.response);
-          }
-        });
+var transporter=nodemailer.createTransport(options,null)
+  
+var mailOptions = {
+  from: 'senders email',
+  to: send,
+  subject: 'testing',
+  text: 'your message',
+  attachments:[
+    {
+      filename:'filename of attachment',
+      path:'path for attachment'
+    }
+  ]
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
--- models/webinarListModel.js
+++ models/webinarListModel.js
@@ -4,7 +4,8 @@ const webinarSchema = new mongoose.Schema({
     name: String,
     eventDate: String,
     description: String,
-    videoLink: String
+    videoLink: String,
+    regLive: Boolean
 });
 
 const Webinar = mongoose.model('webinarList', webinarSchema);
