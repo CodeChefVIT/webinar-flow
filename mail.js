d19aef356d7445bf4c035d3a0cbd86eaa11fd96b
NavyaaSharma <navyaasharma700@rediffmail.com>
2019-10-12 23:38:23 +0530

    event invites added and other changes made

--- mail.js
+++ mail.js
@@ -1,5 +1,11 @@
+var ical = require('ical-generator')
 var nodemailer = require('nodemailer')
 var fs=require('fs')
+var cal=ical()
+
+var path=undefined
+
+// replace the username and password by sender's email and password where ever mentioned as process.env.username and process.env.password
 
 const options={
         pool:true,
@@ -7,22 +13,24 @@ const options={
         port:587,
         secure:false,
         auth:{
-            user:process.env.email,
+            user: process.env.username,
             pass:process.env.password
         }
     }
   
 
-const mail=(mailTo,filename,subject)=>new Promise((resolve,reject)=>{
+const mail=(mailTo,subject,desc)=>new Promise((resolve,reject)=>{
   var transporter=nodemailer.createTransport(options,null)
-
-  var html=fs.readFileSync(filename)
-
+  
   var mailOptions = {
-    from:process.env.sender,
+    from: process.env.username,
     to: mailTo,
     subject,
-    html
+    text: desc ,
+    attachments:[{
+        filename:'invite.ics',
+        path:path
+    }]
   }
 
   transporter.sendMail(mailOptions, function(error, info){
@@ -36,13 +44,40 @@ const mail=(mailTo,filename,subject)=>new Promise((resolve,reject)=>{
   })
 })
 
-const mailMany=(recipients,template,subject)=>{
-  const array=[]
-  for(const i of recipients)
-  {
-    array.push(mail(i,template,subject))
-  }
-  return Promise.all(array)
-}
 
+const mailMany=(send)=>{
+
+  if(send.verified==false)
+  {
+    send.webinarData.description=send.webinarData.description + ". Verify your email using the following link " + send.verificationLink
+  }
+
+  var eventObj = {
+    start : send.webinardata.startTime,
+    end : send.webinarData.endTime,
+    title : send.webinarData.name,
+    description : send.webinarData.description,
+    id : 'wdcwe76234e127eugb', //Some unique identifier
+    organiser : {name : 'CODECHEF-VIT', email:process.env.username}
+  }
+  cal.createEvent({
+    start: eventObj.start,
+    end: eventObj.end,
+    summary: eventObj.title,
+    uid: eventObj.id, // Some unique identifier
+    sequence: 0,
+    description: eventObj.description,
+    organizer: {
+      name: eventObj.organiser.name,
+      email: eventObj.organiser.email
+},
+    method: 'request'
+  });
+
+  //uploads folder has already been made to save invite.ics files to send invites
+  path = __dirname + '/uploads/'+ 'invite' + '.ics';
+  cal.saveSync(path);
+
+  mail(send.email,send.webinarData.name,send.webinarData.description)
+}
 module.exports=mailMany
