435468dd896d50f23cf278bf05f060ecdfa685ac
DiptoChakrabarty <diptochuck123@gmail.com>
2020-01-15 21:22:27 +0530

    build added forcefully

---
+++ client/build/precache-manifest.25fd7477310af3c1352d195e01bfb056.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74dcc5ef312691df62d04c6607949dc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05be3a6498b6bca4d06b",
+    "url": "/static/css/2.e8540187.chunk.css"
+  },
+  {
+    "revision": "ca2c2db2d0dfb2d27e16",
+    "url": "/static/css/main.ae9ea7e3.chunk.css"
+  },
+  {
+    "revision": "05be3a6498b6bca4d06b",
+    "url": "/static/js/2.16cd8a2b.chunk.js"
+  },
+  {
+    "revision": "ca2c2db2d0dfb2d27e16",
+    "url": "/static/js/main.ce517c77.chunk.js"
+  },
+  {
+    "revision": "260f2466768034a29d90",
+    "url": "/static/js/runtime~main.c6ce4a91.js"
+  },
+  {
+    "revision": "54dc7fe247725e5b03bb457b68451076",
+    "url": "/static/media/asm.54dc7fe2.png"
+  },
+  {
+    "revision": "7bb845bf3b38f79dcd922b3801179216",
+    "url": "/static/media/avatar1.7bb845bf.jpg"
+  },
+  {
+    "revision": "6d1d8b6fef333ce48bb316915876b5e8",
+    "url": "/static/media/avatar2.6d1d8b6f.jpg"
+  },
+  {
+    "revision": "eb29f9dc797fb63925b7a95afd5b79cf",
+    "url": "/static/media/cobain.eb29f9dc.jpg"
+  },
+  {
+    "revision": "a3c9b2752e40a10774f33c4f27a4711f",
+    "url": "/static/media/pablo-sign-in.a3c9b275.png"
+  },
+  {
+    "revision": "d42162e4d047026738310d2f9f6becf7",
+    "url": "/static/media/support-notes-colour.d42162e4.svg"
+  },
+  {
+    "revision": "5da640a22164f8ba28dd98e637f0afd0",
+    "url": "/static/media/undraw_programmer_imem.5da640a2.svg"
+  }
+]);
